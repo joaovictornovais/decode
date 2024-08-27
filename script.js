7abcab36992b93f4e86a2e78a8b0888a2747671d
João Victor <joao.dipaay@gmail.com>
2024-08-27 16:29:33 -0300

    feat: text length should be greater than 1

--- script.js
+++ script.js
@@ -9,26 +9,35 @@ let resultText = document.querySelector(".result-text");
 // U -> nge
 function encrypt() {
     let text = document.querySelector("#text").value;
-    text = text.replace(/a/g, 'al');
-    text = text.replace(/e/g, 'k');
-    text = text.replace(/i/g, 'cd');
-    text = text.replace(/o/g, 'chl');
-    text = text.replace(/u/g, 'ng');
-    noMessages.style.display = 'none';
-    resultArea.style.display = 'inline';
-    resultText.innerHTML = text;
+    if (text.length > 1) {
+        text = text.replace(/a/g, 'al');
+        text = text.replace(/e/g, 'k');
+        text = text.replace(/i/g, 'cd');
+        text = text.replace(/o/g, 'chl');
+        text = text.replace(/u/g, 'ng');
+        noMessages.style.display = 'none';
+        resultArea.style.display = 'inline';
+        resultText.innerHTML = text;
+    } else {
+        window.alert("Sua frase deve ter o tamanho maior do que 1")
+    }
 }
 
 function decrypt() {
     let text = document.querySelector("#text").value;
-    text = text.replace(/al/g, 'a');
-    text = text.replace(/k/g, 'e');
-    text = text.replace(/cd/g, 'i');
-    text = text.replace(/chl/g, 'o');
-    text = text.replace(/ng/g, 'u');
-    noMessages.style.display = 'none';
-    resultArea.style.display = 'inline';
-    resultText.innerHTML = text;
+    if (text.length > 1) {
+        text = text.replace(/al/g, 'a');
+        text = text.replace(/k/g, 'e');
+        text = text.replace(/cd/g, 'i');
+        text = text.replace(/chl/g, 'o');
+        text = text.replace(/ng/g, 'u');
+        noMessages.style.display = 'none';
+        resultArea.style.display = 'inline';
+        resultText.innerHTML = text;
+    } else {
+        window.alert("Sua frase deve ter o tamanho maior do que 1")
+    }
+    
 }
 
 function copy() {
